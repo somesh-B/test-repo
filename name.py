4a446efa11fab0fe7efa46965ae1b592e9dff047
somesh B <somesh220f@gmail.com>
2023-12-29 13:03:31 +0530

    modified for wishing customers

--- name.py
+++ name.py
@@ -1,3 +1,4 @@
+print("welcome to our hotel")
 name=input("Enter ur name ")
 print(f"Hi {name} ,Greetings!!")
 hungry=(input("Are you hungry "))
@@ -5,5 +6,3 @@ if hungry == "yes":
     print(f"ok {name} please wait 10 minutes")
 else:
     print(f"ok {name},Njoy ur time")
-
-
